8811118eb51137c657a10b28886eb4f11334fe67
wesley rios <wes.alves.rios@gmail.com>
2022-03-14 08:35:13 -0300

    corrigindo repositório

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// javascrip para trocar os pokemons, esconder o cartão aberto e mostrar apenas o selecionado.
+
+// para isso vamos trabalhar com dois elementos
+// 1 - listagem
+// 2 - cartão pokemon
+
+// precisa criar duas variaveis no js para trabalhar com os elementos na tela.
+// vamos precisar trabalhar com o evento de clique feito pelo usuario na listagem de pokémons
+
+// -remover classe aberto
+// 
